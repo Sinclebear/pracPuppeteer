77c297ca5896d3bdc5339aa767442c5f262fbda2
Sinclebear <sanghyukj@gmail.com>
2022-02-21 15:33:27 +0900

    🚧 homesSchema 정의

---
+++ models/homeSchema.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+
+const homeSchema = mongoose.Schema({
+  home_name: {
+      type: String,
+      default:"",
+  }, 
+  host_name: {
+    type: String,
+    default:"",
+  }, 
+  category: {
+    type: String,
+    default:"",
+  },
+  rateAvg: {
+    type: Number,
+    default:0,
+  },
+  comment_count: {
+    type: Number,
+    default:0,
+  },
+  address: {
+    type: String,
+    default:"",
+  },
+  image_url: {
+    type: [String], // string array 형태이므로 변경. default도 뺌.
+  },
+  introduce: {
+    type: String,
+    default:"",
+  },
+  price: {
+    type: Number,
+    default:0,
+  },
+  convenience: {
+    type: [String], // string array 형태이므로 변경. default도 뺌.
+  },
+  distance: {
+    type: Number,
+    default:0,
+  },
+  availableDate: {
+    type: String,
+    default:"",
+  }
+});
+
+homeSchema.virtual("HomesId").get(function () {
+  return this._id.toHexString();
+});
+
+homeSchema.set("toJSON", {
+  virtuals: true,
+});
+
+module.exports = mongoose.model("Homes", homeSchema);
